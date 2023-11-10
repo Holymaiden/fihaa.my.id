2fb382b43410a0ee16de36d89ff2fbd47533a3f3
Holymaiden <haikalwatampone3@gmail.com>
2023-11-10 21:57:34 +0800

    chore; update footer

--- src/common/components/layouts/partials/Copyright.tsx
+++ src/common/components/layouts/partials/Copyright.tsx
@@ -1,21 +1,35 @@
 const Copyright = () => {
   return (
-    <div className="flex items-center gap-1 text-sm py-1 px-3 text-neutral-600 dark:text-neutral-600 font-sora">
-      <span>©</span>
-      <span>{new Date().getFullYear()}</span>
-      <span>with</span>
-      <span className="text-red-500 animate-pulse">❤</span>
-      <span>by</span>
-      <a
-        href="http://github.com/aulianza/aulianza.id"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <span className="hover:dark:text-neutral-400 cursor-pointer">
-          Fihaa
-        </span>
-      </a>
-    </div>
+    <>
+      <div className="flex items-center gap-1 text-sm py-1 px-3 text-neutral-600 dark:text-neutral-600 font-sora">
+        <span>©</span>
+        <span>{new Date().getFullYear()}</span>
+        <span>with</span>
+        <span className="text-red-500 animate-pulse">❤</span>
+        <span>by</span>
+        <a
+          href="http://github.com/holymaiden"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="hover:dark:text-neutral-400 cursor-pointer">
+            Fihaa
+          </span>
+        </a>
+      </div>
+      <div className="flex items-center gap-1 text-sm py-1 px-3 text-neutral-600 dark:text-neutral-600 font-sora">
+        <div>Template by</div>
+        <a
+          href="http://github.com/aulianza/aulianza.id"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="hover:dark:text-neutral-400 cursor-pointer">
+            Aulianza
+          </span>
+        </a>
+      </div>
+    </>
   );
 };
 
