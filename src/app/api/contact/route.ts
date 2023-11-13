104ee14a342582736cdaa01094e78041955048b7
Holymaiden <haikalwatampone3@gmail.com>
2023-11-13 17:11:24 +0800

    fix(bug): contact api

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,22 +1,25 @@
 import { sendMessage } from "@/services/contact";
-import { NextApiRequest, NextApiResponse } from "next";
 
 const FORM_API_KEY = process.env.CONTACT_FORM_API_KEY as string;
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: Request) {
   try {
-    const { formData } = req.body;
+    const data = await req.json();
 
     const updatedFormData = new FormData();
     updatedFormData.append("access_key", FORM_API_KEY);
 
-    for (const key in formData) {
-      updatedFormData.append(key, formData[key]);
+    for (const key in data) {
+      updatedFormData.append(key, data[key]);
     }
 
     const response = await sendMessage(updatedFormData);
-    res.status(200).json({ status: 200, message: response?.data?.message });
+    return new Response(JSON.stringify(response?.data?.message), {
+      status: 200,
+    });
   } catch (error) {
-    res.status(500).json({ error: "Something went wrong!" });
+    return new Response(JSON.stringify(error), {
+      status: 500,
+    });
   }
 }
