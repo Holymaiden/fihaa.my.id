0f33d650195971d47dd2b23394eed5704387a4db
Holymaiden <haikalwatampone3@gmail.com>
2023-11-22 20:25:36 +0800

    fix(bug): sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,9 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
-      disallow: "/private/",
     },
     sitemap: "https://fihaa.my.id/sitemap.xml",
+    host: "https://fihaa.my.id",
   };
 }
